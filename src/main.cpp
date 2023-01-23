dda044a66b4c2bec9bffcaf99182ca4800af7958
Damien <chickenshalom@gmail.com>
2023-01-23 12:08:23 -0500

    implement shader error handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,8 @@
 #include <iostream>
 
 #include "linmath.h"
- 
-#include <stdlib.h>
-#include <stdio.h>
- 
+#include "shader.h"
+
 static const struct
 {
     float x, y;
@@ -18,29 +16,9 @@ static const struct
     {   0.f,  0.6f, 0.f, 0.f, 1.f }
 };
  
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
- 
 static void error_callback(int error, const char* description)
 {
-    fprintf(stderr, "Error: %s\n", description);
+    std::cout << "Error: " << description << std::endl;
 }
  
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -52,7 +30,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 int main(void)
 {
     GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLuint vertex_buffer;
     GLint mvp_location, vpos_location, vcol_location;
  
     glfwSetErrorCallback(error_callback);
@@ -76,35 +54,22 @@ int main(void)
     glewInit();
     glfwSwapInterval(1);
  
-    // NOTE: OpenGL error checks have been omitted for brevity
- 
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
  
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
- 
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
- 
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
- 
+    ShaderProgram shaderProgram("shaders/vertex.glsl", "shaders/fragment.glsl");
+	GLuint program = shaderProgram.GetProgram();
+	
     mvp_location = glGetUniformLocation(program, "MVP");
     vpos_location = glGetAttribLocation(program, "vPos");
     vcol_location = glGetAttribLocation(program, "vCol");
  
     glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) 0);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*) 0);
+
     glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) (sizeof(float) * 2));
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*) (sizeof(float) * 2));
  
     while (!glfwWindowShouldClose(window))
     {
---
+++ src/shader.cpp
@@ -0,0 +1,67 @@
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+#include "shader.h"
+
+void getFileSource(const std::string &filename, std::string &source_text) {
+	std::ifstream file(filename);
+		
+	std::string line;
+	while (std::getline(file, line)) {
+		source_text += line + "\n";
+	}
+}
+
+void checkShaderErrors(const std::string &filename, GLuint shader) {
+	GLint isCompiled = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+	if(isCompiled == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(shader, maxLength, &maxLength, &errorLog[0]);
+
+		std::cerr << "error compiling shader " << filename << std::endl;
+		std::cerr << &errorLog[0] << std::endl;
+		exit(1);
+	}
+}
+
+ShaderProgram::ShaderProgram(const std::string &vs_filename, const std::string &fs_filename) :
+	uniform_locations()
+{
+	std::string vertex_source, fragment_source;
+	getFileSource(vs_filename, vertex_source);
+	getFileSource(fs_filename, fragment_source);
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	const char* p = vertex_source.c_str();
+    glShaderSource(vertex_shader, 1, &p, NULL);
+    glCompileShader(vertex_shader);
+	checkShaderErrors(vs_filename, vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	p = fragment_source.c_str();
+    glShaderSource(fragment_shader, 1, &p, NULL);
+    glCompileShader(fragment_shader);
+	checkShaderErrors(fs_filename, fragment_shader);
+
+	program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+}
+
+GLuint ShaderProgram::GetProgram() {
+	return program;
+}
+
+ShaderProgram::~ShaderProgram() {
+	glDeleteProgram(program);
+}
